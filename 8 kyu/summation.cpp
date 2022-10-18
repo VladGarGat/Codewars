37cb1b5d4f2051185e560c20d604534a37386bc0
Vlad <96255646+VladGarGat@users.noreply.github.com>
2022-10-18 11:36:55 +0300

    Update summation.cpp

--- 8 kyu/summation.cpp
+++ 8 kyu/summation.cpp
@@ -1,1 +1,13 @@
-// to do
+#include <iostream>
+
+int summation(int num)
+{
+	int result = 0;
+	while (num > 0)
+	{
+		result += num--;
+	}
+	return result;
+}
+
+int main() { int n; std::cin >> n; std::cout << summation(n); }
